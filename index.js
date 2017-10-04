a3e304882dcf2608818caca4455152c370899217
Olli Antila <antila@MCAREPRO-OLLI-MB.local>
2017-10-04 17:19:42 +0300

    bodyparseri

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ var express = require('express');
 var http = require('http');
 
 var server = express();
+var bodyParser = require('body-parser');
+//var multer = require('multer'); // v1.0.5
+//var upload = multer(); // for parsing multipart/form-data
 
+server.use(bodyParser.json()); // for parsing application/json
+server.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+/*
 server.post('/', function (req, res) {
     res.status(200).json({ok: "true", "attachments": [
         {
@@ -16,6 +23,12 @@ server.post('/', function (req, res) {
             "ts": 123456789
         }
     ]});
+});*/
+
+server.post('/', function (req, res) {
+    res.contentType('application/json');
+    console.log(req.body);
+    res.status(200).json({ok: "true", text: "testki"});
 });
 
 server.post('/jee', function (req, res) {
