a737a602bba7a8280e84da6c42a47fa8bc594d6c
Olli Antila <antila@MCAREPRO-OLLI-MB.local>
2017-10-04 17:22:00 +0300

    f

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ server.post('/', function (req, res) {
 server.post('/', function (req, res) {
     res.contentType('application/json');
     console.log(req.body);
-    res.status(200).json({ok: "true", text: "testki"});
+    res.status(200).json({ok: "true", text: req.body});
 });
 
 server.post('/jee', function (req, res) {
