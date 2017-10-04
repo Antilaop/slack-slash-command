6f9da83b3aad300a5a0a34502b4b363031c5e793
Olli Antila <antila@MCAREPRO-OLLI-MB.local>
2017-10-04 16:05:15 +0300

    safd

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
+var express = require('express');
+
 var http = require('http');
 
-var server = http.createServer(function(request, response) {
+var server = express();
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+server.get('/', function (req, res) {
+    res.status(200).json({text: "jee"});
+});
 
+server.get('/jee', function (req, res) {
+    res.json({
+        "attachments": [
+            {
+                "fallback": "Required plain-text summary of the attachment.",
+                "color": "#36a64f",
+                "title": "Slack API Documentation",
+                "title_link": "https://api.slack.com/",
+                "footer": "Slack API",
+                "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
+                "ts": 123456789
+            }
+        ]
+    });
 });
 
 var port = process.env.PORT || 1337;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
+
+
