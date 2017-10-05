bf6a7e004dc33ff8a66715f17df70465a63e34ba
Olli Antila <antila@MCAREPRO-OLLI-MB.local>
2017-10-05 15:36:37 +0300

    json obj

--- index.js
+++ index.js
@@ -14,25 +14,28 @@ server.post('/', upload.array(), function (req, res) {
     var regex = /T20\d{6}\.\d{4}/g;
     var regexArray = regex.exec(req.body.text);
     timeStamp = Math.round(new Date().getTime()/1000);
-    console.log(timeStamp);
     if(regexArray){
         for(var i=0; j=regexArray.length,i<j; i++){
+            var resData = {};
+            var resAttachments = regexArray[i];
+            resData[resAttachments] = [];
+
             var ticketLink = "https://ww4.autotask.net/Autotask/AutotaskExtend/ExecuteCommand.aspx?Code=OpenTicketDetail&TicketNumber=" + regexArray[i];
-            res.status(200).json({
-                "response_type": "in_channel",
-                "attachments": [
-                    {
-                        "fallback": "Required plain-text summary of the attachment.",
-                        "color": "#36a64f",
-                        "title": resTitle,
-                        "title_link": ticketLink,
-                        "footer": "Slack API",
-                        "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
-                        "ts": timeStamp
-                    }
-                ]
-            });
+            
+            var resAttachmentsData = {
+                "fallback": "Required plain-text summary of the attachment.",
+                "color": "#36a64f",
+                "title": regexArray[i],
+                "title_link": ticketLink,
+                "footer": "Slack API",
+                "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
+                "ts": timeStamp
+            }
+
+            resData[resAttachments].push(resAttachmentsData);
         }
+        JSON.stringify(resData);
+        res.status(200).json(resData);
     } else {
         res.status(200).json(
             {
