e6d1ef0dc4082681ecc10e07c5f1cc3db28d8991
Olli Antila <antila@MCAREPRO-OLLI-MB.local>
2017-10-06 15:02:03 +0300

    thumbs up

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ server.post('/', upload.array(), function (req, res) {
                         "title": resTitle,
                         "title_link": ticketLink,
                         "footer": "Kikkare",
-                        "footer_icon": "https://www.colourbox.com/preview/7508808-big-like-icon.jpg",
+                        "footer_icon": "http://www.clker.com/cliparts/c/8/f/8/11949865511933397169thumbs_up_nathan_eady_01.svg.hi.png",
                         "ts": timeStamp
                     }
                 ]
